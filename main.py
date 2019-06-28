c461b0916d3c4117ced570a930f31cd470d6b25c
watosar2113@gmail.com <watosar2113@gmail.com>
2019-06-28 15:37:48 +0000

    modified main.py

--- main.py
+++ main.py
@@ -1,38 +1,37 @@
 import ui
 from objc_util import *
-from viewcontroller import *
+from viewcontroller import ViewController, renderer
 import time
 import os
+
 load_framework('Metal')
 MTKView = ObjCClass('MTKView')
+
 
 MTLCreateSystemDefaultDevice = c.MTLCreateSystemDefaultDevice
 MTLCreateSystemDefaultDevice.restype = c_void_p
 def mtl_create_system_default_device():
     return ObjCInstance(MTLCreateSystemDefaultDevice())
 
+
 def create_view(x, y, w, h):
     view = MTKView.alloc().initWithFrame_device_((CGRect(CGPoint(x, y), CGSize(w, h))), mtl_create_system_default_device())
     return view
     
 
-sceneview = None
-cvc = None
-
 @on_main_thread
 class MyView(ui.View):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.sceneview = None
+        self.cvc = None
+        
     def initialize(self):
-        global sceneview, cvc
-        
-        screen = ui.get_screen_size()
+        #screen = ui.get_screen_size()
         #sceneview = create_view(0, 0, screen.width, screen.height)
-        sceneview = create_view(0, 0, 375, 375)
-        cvc = ViewController.new().autorelease()
-        
-        cvc.view = sceneview
-        #cvc.view().setShowsStatistics_(True)
-    
-        print('is loaded?', cvc.isViewLoaded())
+        sceneview = create_view(0, 110, 375, 375)
+        self.cvc = cvc = ViewController.new().autorelease()
+        self.sceneview = cvc.view = sceneview
         self_objc = ObjCInstance(self)
         self_objc.nextResponder().addChildViewController_(cvc)
         self_objc.addSubview_(sceneview)
@@ -43,10 +42,7 @@ class MyView(ui.View):
         os._exit(0)
 
 
-
-my_view = MyView()
-
-
+my_view = MyView(background_color=(0,.3,0,1))
 my_view.present('full_screen', hide_title_bar=False,)
 my_view.initialize()
 
@@ -57,10 +53,10 @@ current_time = seek_bar_view['current_time']
 button = seek_bar_view['button']
 
 play_flg = True
-max_time = 3
-s_time = py_renderer.s_time
+max_time = 10
+s_time = renderer.s_time
 
-def seek_action(_=None):
+def seek_action(*_):
     global s_time, play_flg
     play_flg = False
     button.image = ui.Image.named('iob:play_256')
@@ -85,16 +81,9 @@ def time_controlle():
 def switch_play_flg(_):
     global play_flg
     play_flg = not play_flg
-    if play_flg:
-        image = ui.Image.named('iob:pause_256')
-    else:
-        image = ui.Image.named('iob:play_256')
-    button.image = image
+    button.image = ui.Image.named('iob:{}_256'.format(('play','pause')[play_flg]))
 
 seek_bar.action = seek_action
-py_renderer.get_time = time_controlle
+renderer.get_time = time_controlle
 button.action = switch_play_flg
-
-my_view.bg_color = (0,0.3,0,1)
-sceneview.setPosition_((187.5,300))
 
